7dc35d948ad974b947ad469561e26d064c309e58
Suman <sumanmukherjee490@gmail.com>
2021-06-17 20:57:57 +0530

    Data

---
+++ cucumbor/src/test/java/QUIBDD/cucumbor/AppTest.java
@@ -0,0 +1,31 @@
+package QUIBDD.cucumbor;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Given("Open the chrome and launch the application")
+	public void open_the_chrome_and_launch_the_application() {
+System.out.println("JAVA");
+
+	}
+
+	@When("Enter the Username and Password")
+	public void enter_the_username_and_password() {
+		System.out.println("JAdddVA");
+		
+	}
+
+	@Then("Reset the credential")
+	public void reset_the_credential() {
+		System.out.println("JAVioA");
+		
+	}
+	
+
+}
